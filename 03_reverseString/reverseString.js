d1d9d272320e0b48e9cfa53ec1aa614c95e8cdd0
Ibra <lomeseth1@gmail.com>
2022-10-28 16:51:22 +0200

    initial sol.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let temp ="";
+for(let i = str.length-1; i>=0;i--)
+    temp += str.charAt(i);
+return temp;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let remove;
+    for(let i = 0;i<arr.length;i++){
+        remove = false;
+        for(let j = 1;j<arguments.length;j++){
+            if(arguments[j] === arr[i]){
+                remove = true;
+            }
+        }
+        if(remove === true){
+            arr.splice(i,1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
